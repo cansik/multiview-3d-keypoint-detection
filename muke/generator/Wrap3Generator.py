5f8fc649199ec419fdf283e0bd1ef269026f6a0b
Florian Bruggisser <florian@bildspur.ch>
2021-04-27 17:49:46 +0200

    fixed wrap3 output

--- muke/generator/Wrap3Generator.py
+++ muke/generator/Wrap3Generator.py
@@ -8,7 +8,7 @@ from muke.model import KeyPoint3
 class Wrap3Generator(BaseGenerator):
     def generate(self, input_path: str, keypoints: [KeyPoint3]):
         # create txt file besides input mesh
-        keypoint_file_name = "%s_keypoints.json" % os.path.splitext(input_path)[0]
+        keypoint_file_name = "%s_keypoints.txt" % os.path.splitext(input_path)[0]
         output = json.dumps([{"x": kp.x, "y": kp.y, "z": kp.z} for kp in keypoints], indent=4, sort_keys=True)
         with open(keypoint_file_name, "w") as file:
             file.write(output)
